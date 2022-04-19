f4b3d9e0818f0e42858a7113d9660d34ae08c1f5
Camilo Cuitiva <camiloandres174@gmail.com>
2022-04-19 15:49:34 -0500

    Add Project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'Softblue': "hsl(215, 51%, 70%)",
+        'Cyan': "hsl(178, 100%, 50%)",
+        'Verydarkblue(mainBG)': 'hsl(217, 54%, 11%)',
+        'Verydarkblue(cardBG)': 'hsl(216, 50%, 16%)',
+        'Verydarkblue(line)': 'hsl(215, 32%, 27%)',
+        'White': 'hsl(0, 0%, 100%)',
+
+      }
+    },
+  },
+  plugins: [],
+}
